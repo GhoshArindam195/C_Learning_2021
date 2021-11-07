99d2c6fe647f0b5722a97f66b3bf45c9035b06ef
Arindam <ghosharindam195@gmail.com>
2021-11-07 15:46:55 +0530

    07-11-21

---
+++ bitWiseExample.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a= 12;
+    int b= 17;
+
+    printf("%d\n", a|b);
+    printf("%d\n", a&b);
+    printf("%d\n", a^b);
+}
---
+++ preAndpostIncreAnddecrementOperator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    // int i=100;
+    // int j=i++;   //Post increment
+    // printf("%d %d\n", i, j);
+
+    // int i=100;
+    // int j=++i;   //Pre increment
+    // printf("%d %d\n", i, j);
+
+
+    // int i=100;
+    // int j=i--;   //Post decrement
+    // printf("%d %d\n", i, j);
+
+    int i=100;
+    int j=--i;  //Pre decrement
+    printf("%d %d\n", i, j);
+}
