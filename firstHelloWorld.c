8e743d810cc90ab3adf1cfb6d10cd3a25edcd5de
Arindam <ghosharindam195@gmail.com>
2021-11-07 14:04:11 +0530

    First Commit

---
+++ firstHelloWorld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    // This is our first C program....
+    // printf("\nHello World");
+    // printf("\nHello World");
+
+    int n;  //var declaration
+    n=100;  //var initialization
+
+    int m = 200;    //var declaration as well as initialization 
+    // int taniya=10;
+    // float sathi=10.999;
+}
